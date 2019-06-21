3a5ad815278a7500373af093bf3cf5856f26d69a
Diogo Souza <dsouza@vectoritcgroup.com>
2019-06-21 11:24:42 -0300

    injecting ionic controllers

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +13,12 @@ export class RecipeDetailPage implements OnInit {
   loadRecipes: Recipe;
 
   //injetar o servço de receitas
-  constructor(private activateRoute: ActivatedRoute, private recipesService: RecipesService) { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private recipesService: RecipesService,
+    private router: Router,
+    private alertCtrl: AlertController
+    ) { }
 
   ngOnInit() {
     this.activateRoute.paramMap.subscribe(paramMap => {
@@ -25,4 +31,27 @@ export class RecipeDetailPage implements OnInit {
     });
   }
 
+  onDeleteRecipe(){
+    //exibe uma tela de alert (Controlador de alerta)
+    this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete the recipe?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Delete',
+          handler: () => { //Define uma função que é executado quando o botão é pressionado
+            this.recipesService.deleteRecipe(this.loadRecipes.id);
+            this.router.navigate(['/recipes']);
+          }
+        }
+      ]
+    })
+    .then(alertEl => {
+      alertEl.present();
+    });
+  }
 }
